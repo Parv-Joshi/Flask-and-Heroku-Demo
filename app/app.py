4c38c1e0f767ca422885a214e2b13f553b5b14d3
Parv Joshi <pj2384@columbia.edu>
2021-09-01 15:33:50 +0400

    Initial Commit

---
+++ app/app.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, request
+import numpy as np
+from joblib import load
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import uuid
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def hello_world():
+    request_type_str = request.method
+    if request_type_str == "GET":
+        return render_template("index.html", href="static/base_pic.svg")
+    else:
+        text = request.form["text"]
+        random_str = uuid.uuid4().hex
+
+        path = "static/" + random_str + ".svg"
+        model = load("model.joblib")
+        np_arr = floats_string_to_np_arr(text)
+        make_picture("AgesAndHeights.pkl", model, np_arr, path)
+        
+        return render_template("index.html", href=path)
+
+def make_picture(training_data_filename, model, new_inp_np_arr, output_file):
+    df = pd.read_pickle(training_data_filename)
+
+    ages = df["Age"]
+    df = df[ages > 0]
+    ages = df["Age"]
+    heights = df["Height"]
+
+    x_new = np.array(list(range(19))).reshape(-1, 1)
+    preds = model.predict(x_new)
+
+    fig = px.scatter(x=ages, y=heights, title="Height vs. Age", labels={"x":"Age (years)", "y":"Height (inches)"})
+    fig.add_trace(go.Scatter(x=x_new.reshape(-1), y=preds, mode="lines", name="Model"))
+
+    new_preds = model.predict(new_inp_np_arr)
+    fig.add_trace(go.Scatter(x=new_inp_np_arr.reshape(-1), y=new_preds, mode="markers", marker=dict(color="purple", size=15)))
+
+    fig.write_image(output_file, width = 800, engine="kaleido")
+    fig.show()
+
+def floats_string_to_np_arr(floates_str):
+    floats=list()
+
+    for x in floates_str.split(","):
+        try:
+            float(x)
+            floats.append(float(x))
+        except: pass 
+
+    return np.array(floats).reshape(-1, 1)
